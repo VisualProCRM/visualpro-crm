42762ef5727b2293e26d8dd1568496e68089d812
VisualProCRM <enquiries@visualglazing.co.uk>
2026-07-14 09:15:42 +0100

    Update Vite config to specify input HTML file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist' }
+  root: '.',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html'
+    }
+  }
 })
