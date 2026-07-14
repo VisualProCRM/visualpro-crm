597fdacb3ee9d89b7950be0aaed21fa8879e09d7
VisualProCRM <enquiries@visualglazing.co.uk>
2026-07-14 09:26:01 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  publicDir: 'public',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'public/index.html'
-    }
+    outDir: 'dist'
   }
 })
